cf9f3d9847af62d5e4b53bef06579ef6cbe8e1da
Morenok19 <davidgomezmoreno1@gmail.com>
2025-01-29 22:15:04 +0100

    Añado clase resta (a ver si quiere)

---
+++ Calculadora-Actividad2/src/main/java/org/Calculadora/Resta.java
@@ -0,0 +1,38 @@
+package org.Calculadora;
+
+public class Resta {
+
+    //Clase creada por David Gómez Moreno
+
+    private double numReal1;
+    private double numReal2;
+    private double numReal3;
+    private int numInt1;
+    private int numInt2;
+
+
+    //Metodos propios
+
+    //Resta de dos reales
+    public double restaReales(double numReal1, double numReal2) {
+        return numReal1 - numReal2;
+    }
+
+
+    //Resta de dos enteros
+    public double restaEnteros(int numInt1, int numInt2) {
+        return numInt1 - numInt2;
+    }
+
+    //Resta de tres reales
+    public double restaTresReales(double numReal1, double numReal2, double numReal3) {
+        return numReal1 - numReal2 - numReal3;
+    }
+
+    //Resta de valor acumulado
+    public double restaValorAcumulado(double valor) {
+        double valorAcumulado = 0;
+        return valor + valorAcumulado;
+
+    }
+}
