63ea1dbdd0f801db961dcc410ba44fb4019aa595
Francisco Jose Soria <franciscosorianavarrete@gmail.com>
2025-02-08 16:36:05 +0100

    Actualización de TestMenu aportando mejoras por Fran

--- Calculadora-Actividad2/src/test/java/org/Calculadora/TestMenu.java
+++ Calculadora-Actividad2/src/test/java/org/Calculadora/TestMenu.java
@@ -1,12 +1,7 @@
 package org.Calculadora;
 
-import org.Calculadora.Suma;
-import org.Calculadora.Resta;
-import org.Calculadora.Producto;
-import org.Calculadora.Division;
-import org.Calculadora.Modulo;
-
 import java.util.Scanner;
+
 
 public class TestMenu {
     public static void main(String[] args) {
@@ -14,27 +9,29 @@ public class TestMenu {
 
         char opcion;
         do {
-            System.out.println("¿Qué operación desea realizar?");
+            System.out.println("\n ¿Qué operación desea realizar?");
             System.out.println("Operaciones de tipo Suma (S)");
             System.out.println("Operaciones de tipo Resta (R)");
             System.out.println("Operaciones de tipo Producto (P)");
             System.out.println("Operaciones de tipo División (D)");
             System.out.println("Operaciones de tipo Módulo (M)");
+            System.out.println("SALIDA DEL PROGRAMA (X)");
 
-            opcion = sc.next().charAt(0);
+            opcion = sc.next().toUpperCase().charAt(0);
             switch (opcion) {
                 case 'S':
-                    System.out.println("Suma de dos números reales");
-                    Suma.sumaReales(3.4, 6.5);
+                    System.out.println("Suma de dos números reales: ");
+                    System.out.println(Suma.sumaReales(3.4, 6.5));
 
-                    System.out.println("Suma de dos números enteros");
-                    Suma.sumaEnteros(4, 9);
+                    System.out.println("Suma de dos números enteros: ");
+                    System.out.println(Suma.sumaEnteros(4, 9));
 
-                    System.out.println("Suma de tres números reales");
-                    Suma.sumaTresReales(6.5, 8.1, 6.4);
+                    System.out.println("Suma de tres números reales: ");
+                    System.out.println(Suma.sumaTresReales(6.5, 8.1, 6.4));
+                    System.out.println("Suma con valor acumulado: ");
+                    System.out.println(Suma.sumaConAcumulado(8.2));
 
-                    System.out.println("Suma con valor acumulado");
-                    Suma.sumaConAcumulado(8.2);
+                    break;
 
                 case 'R':
                     System.out.println("Resta de dos números reales");
@@ -47,9 +44,11 @@ public class TestMenu {
                     Resta.restaTresReales(12.5, 5.6, 1.3);
 
                     //System.out.println("Resta con valor acumulado");
-                    //Resta.restaValorAcumulado(6);
+                    //
 
-                case 'P':
+                    break;
+
+              /*  case 'P':
                     System.out.println("Resta de dos números reales");
                     Resta.restaReales(6.5, 2.6);
 
@@ -62,11 +61,19 @@ public class TestMenu {
                     //System.out.println("Resta con valor acumulado");
                     //Resta.restaValorAcumulado(6);
 
+                    break;*/
+                case 'X':
+                    System.out.println("SALIENDO");
+                    break;
+                default:
+                    System.out.println("Opción no válida, prueba otra operación");
+
             }
 
 
         } while (opcion != 'X');
-
+        sc.close();
+        System.out.println("FIN DEL PROGRAMA");
 
     }
 
