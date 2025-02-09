050fc5a932a052888d1add1c9dbedbaa2331b05c
Alberto Balaguer <alberto.balaguer184@comunidadunir.netgit config --global user.emailgit config --global user.email alberto.balaguer184@comunidadunir.net>
2025-02-09 19:27:46 +0100

    Restitución de TestResta.

--- Calculadora-Actividad2/src/test/java/org/Calculadora/TestResta.java
+++ Calculadora-Actividad2/src/test/java/org/Calculadora/TestResta.java
@@ -2,12 +2,5 @@ package org.Calculadora;
 
 public class TestResta {
     public static void main(String[] args) {
-
-        //Probamos el metodo Resta:
-        System.out.println(Resta.restaEnteros(9,4));
-        System.out.println(Resta.restaReales(3.5,2.25));
-        System.out.println(Resta.restaTresReales(3.5,2.25,0.5));
-
-
     }
 }
