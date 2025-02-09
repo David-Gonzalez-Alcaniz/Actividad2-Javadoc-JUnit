08a0d46672f5aca161043dc32ab7c8dbd2f2c1ec
Alberto Balaguer <alberto.balaguer184@comunidadunir.netgit config --global user.emailgit config --global user.email alberto.balaguer184@comunidadunir.net>
2025-02-09 22:23:34 +0100

    Nuevo ModuloTest (en construcción). Falta dar solución a la excepción.

--- Calculadora-Actividad2/src/test/java/org/Calculadora/TestModulo.java
+++ Calculadora-Actividad2/src/test/java/org/Calculadora/TestModulo.java
@@ -1,7 +1,39 @@
 package org.Calculadora;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class TestModulo {
     public static void main(String[] args) {
 
+
+        //PRUEBA TEST DE CLASE MÓDULO:
+
+        //Módulo de la división de dos números enteros:
+        System.out.println(Modulo.calculoModulo(359,27));
+
+        //Módulo de la división entre 2 enteros, siendo int b = 0:
+        System.out.println(Modulo.calculoModulo(9,0));
+
+        //Valor absoluto de número real:
+        System.out.println(Modulo.calculoValorAbsoluto(2.5));
+    }
+
+    @Test
+    void calculoModulo() {
+        Modulo modulo1 = new Modulo(359,27);
+        assertAll(
+                () -> assertEquals(8, modulo1.calculoModulo(359, 27))
+        );
+    }
+
+
+    @Test
+    void calculoValorAbsoluto() {
+        Modulo modulo3 = new Modulo();
+        assertAll(
+                () -> assertEquals(2.5, modulo3.calculoValorAbsoluto(2.5))
+        );
     }
 }
