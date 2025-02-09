9ea0b1946f941e20c21aebdd3a70648f9fe1ee7f
Zaira Gonzalez <zairagonzalezencabo@gmail.com>
2025-02-09 19:18:36 +0100

    Actualización Zaira clase Modulo JavaDoc

--- Calculadora-Actividad2/src/main/java/org/Calculadora/Modulo.java
+++ Calculadora-Actividad2/src/main/java/org/Calculadora/Modulo.java
@@ -1,69 +1,111 @@
 package org.Calculadora;
- // CLASE ZAIRA GONZALEZ --> MODULO
+/**
+ * La clase Modulo proporciona operaciones relacionadas con el módulo (residuo de una división)
+ * y el valor absoluto.
+ *
+ * @author [Zaira Gonzalez Encabo]
+ * @version 1.0
+ */
 public class Modulo {
-	/*
-	Esta clase proporciona operaciones relacionadas con el módulo (residuo de una división) y el valor
-	absoluto.
-	*/
-
+    /**
+     * Primer numero entero
+     */
     private int a;
+    /**
+     * Segundo numero entero
+     */
     private int b;
 
+    /**
+     *  Atributos de la clase:
+     *
+     * @param a     Primer número
+     * @param b     Segundo número
+     */
+
+    /**
+     *  Constructor con todos los parámetros
+     */
     public Modulo(int a, int b) {
         super();
         this.a = a;
         this.b = b;
     }
 
+    /**
+     *  Constructor sin parámetros
+     */
     public Modulo() {
         super();
     }
 
-    public int geta() {
+    /**
+     *  Getters and setters
+     */
+
+    /**
+     * Devuelve el valor asignado al atributo a
+     * @return El valor de a
+     */
+    public int getA() {
         return a;
     }
 
+    /**
+     * Establece el valor de a
+     * @param a Nuevo valor para a
+     */
     public void setA(int a) {
         this.a = a;
     }
 
+    /**
+     * Devuelve el valor asignado al atributo b
+     * @return El valor de b
+     */
     public int getB() {
         return b;
     }
 
+    /**
+     * Establece el valor de b
+     * @param b Nuevo valor para b
+     */
     public void setB(int b) {
         this.b = b;
     }
 
+    /**
+     *  Método toString
+     */
     @Override
     public String toString() {
         return "claseEj [a=" + a + ", b=" + b +  "]";
     }
 
     //METODOS
-	/*
-	 * 1. Cálculo del módulo (residuo de una división entera).
-	o Entrada: int a, int b
-	o Salida: int (el residuo de la división de a entre b)
-	o Fórmula:
-	▪ Residuo: M = a % b
-	 */
 
+    /**
+     * Calcula el módulo (residuo) de la división entre dos números enteros:
+     *
+     * @param a Primer número entero
+     * @param b Segundo número entero
+     * @return El residuo de la división entera de a entre b
+     * @throws ArithmeticException Division entre 0 NO permitido
+     */
     public static int calculoModulo(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("División entre 0 NO permitido");
         }
         return a % b;
     }
-	/*
-	 * 2. Cálculo del valor absoluto de un número.
-	o Entrada: double a
-	o Salida: double (el valor absoluto de a)
-	o Fórmula:
-	▪ Si a ≥ 0, entonces |a| = a
-	▪ Si a < 0, entonces |a| = -a
-	*/
 
+    /**
+     * Calcula el valor absoluto de un número real:
+     *
+     * @param a Número real
+     * @return El valor absoluto de a
+     */
     public static double calculoValorAbsoluto(double a) {
         if (a >= 0)
             return a;
