33070f0139dd3490939d809c5968f3f13a841feb
Francisco Jose Soria <franciscosorianavarrete@gmail.com>
2025-02-11 10:55:52 +0100

    Commit del test Division por Fran ya testeado

--- Calculadora-Actividad2/src/test/java/org/Calculadora/TestDivision.java
+++ Calculadora-Actividad2/src/test/java/org/Calculadora/TestDivision.java
@@ -1,14 +1,39 @@
 package org.Calculadora;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class TestDivision {
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-        //Prueba test de metodo DIVISION
+    System.out.println(Division.cocienteEnteros(9,4));
 
-        System.out.println(Division.cocienteEnteros(9,4));
+    System.out.println(Division.cocienteReales(3.5,2.25));
 
-        System.out.println(Division.cocienteReales(3.5,2.25));
+    System.out.println(Division.numeroInverso(5.5));
+            }
 
-        System.out.println(Division.numeroInverso(5.5));
+    private Division division1;
+
+
+    @BeforeEach
+    void setUp() {
+        division1 = new Division();
+    }
+
+    @Test
+    void cocienteReales() {
+        assertEquals(1.5555555555555556,division1.cocienteReales(3.5,2.25));
+    }
+
+    @Test
+    void cocienteEnteros() {
+        assertEquals(2.25,division1.cocienteEnteros(9,4));
+    }
+
+    @Test
+    void numeroInverso() {
+        assertEquals(0.18181818181818182,division1.numeroInverso(5.5));
     }
 }
